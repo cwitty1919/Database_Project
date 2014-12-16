60ecc6aa8eec0065e47811e8149cee283f960b7d
Michael Asnes <mike.asnes@gmail.com>
2014-12-15 19:12:25 -0700

    moved databases.html -> databases.php

--- databases.php
+++ databases.php
@@ -1,51 +1,16 @@
-<?php
-// Basic php databases example
+<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+   <head>
+       <meta charset="utf-8">
+      <title> database project </title>
+      <meta name="database project" content="Project for CU Databases course - boardgame tournament database">
+   </head>
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully\n";
-
-// Create database
-$dbname = myDB;
-$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created/already exists\n";
-} else {
-  echo "Error creating database: " . $conn->error;
-}
-
-$conn->close();
-echo "Connection closed";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connecting to database $dbname";
-
-// sql to create table
-$sql = "CREATE TABLE IF NOT EXISTS MyGuests (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  firstname VARCHAR(30) NOT NULL,
-  lastname VARCHAR(30) NOT NULL,
-  email VARCHAR(50),
-  reg_date TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Table MyGuests created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
-}
-$conn->close();
-echo "Connection closed";
-
-?>
+<body>
+   <h1>database project</h1>
+   <p>by michael asnes and chris wittenberg</p>
+   <p>this is the base webpage for our database project.</p> 
+   <p>Here's the php stuff</p>
+   <?php include 'db.php';?>
+</body>
+</html>
