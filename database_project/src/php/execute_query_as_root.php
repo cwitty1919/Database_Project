03e713f75dbb95ac24f2cba2cd055b803b8fdeb1
Michael Asnes <mike.asnes@gmail.com>
2014-12-16 00:54:51 -0700

    queries executed as root no longer require the db 'project' to be present

--- database_project/src/php/execute_query_as_root.php
+++ database_project/src/php/execute_query_as_root.php
@@ -2,13 +2,6 @@
 include_once 'src/php/dblogin_root.php';
 
 function execute_query($to_execute, $conn) {
-   $query="USE project";
-
-   $result = $conn->query($query);
-
-   if ($result === FALSE)
-      die("Error: failed to connect to the database<br>");
-
    $query="$to_execute";
 
    echo "Executing query: '$query' <br><br>";
