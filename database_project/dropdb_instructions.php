c2e97cd4af16ef3b5187ac2d6f14413f2772cc54
Michael Asnes <mike.asnes@gmail.com>
2014-12-15 19:32:57 -0700

    added a page which drops the database

---
+++ database_project/dropdb_instructions.php
@@ -0,0 +1,11 @@
+<?php
+include 'dblogin.php';
+
+$dropdb = "DROP DATABASE IF EXISTS $dbname;";
+if ($conn->query($dropdb) !== TRUE) {
+    $deathstring = "Error dropping database: $conn->error <br>";
+    die($deathstring);
+}
+
+echo "Database dropped!";
+?>
